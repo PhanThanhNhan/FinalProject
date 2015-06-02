d9b1b2b19bb735c31bb2e34f4bad0f46ac5e390c
PhanThanhNhan <brucelee_4ever@yahoo.com.vn>
2015-06-02 08:13:39 +0700

    Add map to ChoostOnMap function

--- app/src/main/java/com/example/youneverknow/finalproject/ChooseOnMap/ChooseOnMapActivity.java
+++ app/src/main/java/com/example/youneverknow/finalproject/ChooseOnMap/ChooseOnMapActivity.java
@@ -4,15 +4,37 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import com.example.youneverknow.finalproject.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by YouNeverKnow on 5/31/2015.
  */
 public class ChooseOnMapActivity extends FragmentActivity {
 
+    private Marker marker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseonmap);
+
+        final GoogleMap googleMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.googleMap )).getMap();
+        //googleMap.animateCamera(CameraUpdateFactory.newLatLng(location), 1000, null);
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                googleMap.clear();
+                marker = googleMap.addMarker(new MarkerOptions().position(latLng));
+            }
+        });
+
     }
+
+
 }
